14c23bc383192dd348a757ff187f6abb2b91cfda
eunseo <eunseo.choi.d@gmail.com>
2021-09-04 12:20:16 +0900

    Add comments

--- tcp/client.go
+++ tcp/client.go
@@ -7,16 +7,17 @@ import (
 )
  
 func main() {
-    conn, err := net.Dial("tcp", "127.0.0.1:8000")
+    conn, err := net.Dial("tcp", "127.0.0.1:8000") // 프로토콜, IP 주소, 포트 번호를 설정하여 서버에 연결
     if err != nil {
-        fmt.Println("Faield to Dial : ", err)
+        fmt.Println("Failed to Dial : ", err)
     }
-    defer conn.Close()
+    defer conn.Close() // TCP 연결을 닫음
  
     go func(c net.Conn) {
         send := "Hello"
+		
         for {
-            _, err = c.Write([]byte(send))
+            _, err = c.Write([]byte(send)) // server로 데이터를 보냄
             if err != nil {
                 fmt.Println("Failed to write data : ", err)
                 break;
@@ -30,9 +31,9 @@ func main() {
         recv := make([]byte, 4096)
  
         for {
-            n, err := c.Read(recv)
+            n, err := c.Read(recv) // server에게 받은 데이터를 읽음
             if err != nil {
-                fmt.Println("Failed to Read data : ", err)
+                fmt.Println("Failed to read data : ", err)
                 break
             }
  
--- tcp/server.go
+++ tcp/server.go
@@ -7,10 +7,11 @@ import (
 )
  
 func handler(conn net.Conn) {
+	fmt.Println("=== Run handler")
     recv := make([]byte, 4096)
  
     for {
-        n, err := conn.Read(recv)
+        n, err := conn.Read(recv) // client에게 받은 데이터를 읽음
         if err != nil {
             if err == io.EOF {
                 fmt.Println("connection is closed from client : ", conn.RemoteAddr().String())
@@ -21,20 +22,20 @@ func handler(conn net.Conn) {
  
         if n > 0 {
             fmt.Println(string(recv[:n]))
-            conn.Write(recv[:n])
+            conn.Write(recv[:n]) // 받은 데이터를 client에게 다시 보냄
         }
     }
 }
  
 func main() {
-    l, err := net.Listen("tcp", ":8000")
+    l, err := net.Listen("tcp", ":8000") // 프로토콜, IP 주소, 포트 번호를 설정하여 네트워크 연결을 대기
     if err != nil {
         fmt.Println("Failed to Listen : ", err)
     }
-    defer l.Close()
+    defer l.Close() // TCP 연결 대기를 닫음
  
     for {
-        conn, err := l.Accept()
+        conn, err := l.Accept() // 클라이언트가 연결되면 커넥션을 리턴
         if err != nil {
             fmt.Println("Failed to Accept : ", err)
             continue
